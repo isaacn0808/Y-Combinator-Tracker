cad0c97eb4f2d87c6af91c9cfee730957f88e172
isaac <isaacn0808@gmail.com>
2025-05-26 02:40:32 -0500

    index healthchechk

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,6 @@ app.use(cors());
 
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from YC Tracker Backend (Express)!');
   res.status(200).json({ status: 'healthy' });
 });
 
